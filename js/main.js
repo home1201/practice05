5cb79dc58e86ea14d75bef2f411865fd3caa4fe6
homeqwert <home1201@gmail.com>
2024-03-25 12:37:38 +0900

    접근성 강화 (버튼 수정)

--- js/main.js
+++ js/main.js
@@ -4,11 +4,14 @@ import '/sass/style.scss';
   const accordionButtonArr = [...document.querySelectorAll('[data-accordion-button]')];
   accordionButtonArr.forEach(e => {
     e.addEventListener('click', (ev) => {
-      const accordionItem = ev.target.closest('[data-accordion-item]');
-      const destElArr = [accordionItem, ...accordionItem.querySelectorAll('[data-state]')];
+      const accordionItemEl = ev.currentTarget.closest('[data-accordion-item]');
+      const destElArr = [accordionItemEl, ...accordionItemEl.querySelectorAll('[data-state]')];
       destElArr.forEach(e => {
         e.dataset.state = (e.dataset.state === 'opened') ? "closed" : "opened";
       });
+
+      const textEl = ev.currentTarget.querySelector('[data-accordion-button-text]');
+      textEl.textContent = (textEl.dataset.state === 'opened') ? "접기" : "펼치기";
     });
   });
 })();
